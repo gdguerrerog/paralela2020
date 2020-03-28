c4bc69caf14bd52c9ae22737d6e095f64c14207c
German David Guerrero Guerrero <gdguerrerog@unal.edu.co>
2020-03-28 11:30:52 -0500

    :file_folder: file system done

--- practica1/blur_effect.c
+++ practica1/blur_effect.c
@@ -1,43 +1,19 @@
-#include <stdint.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "libs/stb/stb_image.h"
-
-typedef struct Pixel{
-    unsigned char R, G, B;
-} Pixel;
+#include "file_system.h"
 
 int main() {
-    int width, height, bpp;
-
-    unsigned char *rgb_image = stbi_load("LittleRGB.jpg", &width, &height, &bpp, STBI_rgb);
-
-    if(rgb_image == NULL){
+    Image img;
+    if(!readImage("LittleBlue.jpg", &img)){
         printf("ERROR LOADING IMAGE\n");
         return 0;
     }
 
-    printf("width: %d, height: %d, bpp: %d\n", width, height, bpp);
-
-    Pixel image[height][width];
-    for(int i = 0; i < width*height; i++){
-            image[i/height][i % height].R = rgb_image[3*i]; 
-            image[i/height][i % height].G = rgb_image[3*i + 1]; 
-            image[i/height][i % height].B = rgb_image[3*i + 2];
-            
-    }
-
-    stbi_image_free(rgb_image);
-
-
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
-            printf("[%03d;%03d;%03d] ", image[i][j].R, image[i][j].G, image[i][j].B);
-        }
-
-        printf("\n");
-    }
-
+    printImage(&img);
+    img.pixels[2][2].R = 0xFF;
+    img.pixels[2][2].G = 0xFF;
+    img.pixels[2][2].B = 0xFF;
+    printImage(&img);
+    writeImage(&img, "image_created.png");
     
-
+    freeImage(&img);
     return 0;
 }
---
+++ practica1/file_system.h
@@ -0,0 +1,72 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "libs/stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "libs/stb/stb_image_write.h"
+
+typedef struct Pixel{
+    uint8_t R, G, B;
+} Pixel;
+
+typedef struct Image{
+    int width, height;
+    Pixel * * pixels;
+} Image;
+
+
+bool readImage(char * name, Image * img){
+    int width, height, bpp;
+
+    uint8_t *rgb_image = stbi_load(name, &width, &height, &bpp, STBI_rgb);
+
+    if(rgb_image == NULL) return false;
+    
+    Pixel * * image = (Pixel * * )malloc(height* sizeof(Pixel *));
+    for(int i = 0; i < height; i++) image[i] = (Pixel *)malloc(width*sizeof(Pixel));
+    
+    for(int i = 0; i < width*height; i++){
+        image[i%height][i /height].R = rgb_image[3*i]; 
+        image[i%height][i /height].G = rgb_image[3*i + 1]; 
+        image[i%height][i /height].B = rgb_image[3*i + 2];    
+    }
+
+    stbi_image_free(rgb_image);
+
+    img->pixels = image;
+    img->width = width;
+    img->height = height;
+
+    return true;
+}
+
+void writeImage(Image * img, char * name){
+
+    int width = img->width, height = img->height;
+    uint8_t* rgb_image = (uint8_t*)malloc(width*height*3);
+
+    for(int i = 0; i < width*height; i++){
+        rgb_image[3 * i] = img->pixels[i/3][i % 3].R;
+        rgb_image[3 * i + 1] = img->pixels[i/3][i % 3].G;
+        rgb_image[3 * i + 2] = img->pixels[i/3][i % 3].B;
+    }
+
+    stbi_write_png(name, width, height, 3, rgb_image, width*3);
+}
+
+void printImage(Image * img){
+    for(int i = 0; i < img->height; i++){
+        for(int j = 0; j < img -> width; j++){
+            printf("[%03d;%03d;%03d] ", img->pixels[i][j].R, img->pixels[i][j].G, img->pixels[i][j].B);
+        }
+
+        printf("\n");
+    }
+}
+
+void freeImage(Image * img){
+    for(int i = 0 ; i < img->height; i++) free(img->pixels[i]);
+    free(img->pixels);
+}
